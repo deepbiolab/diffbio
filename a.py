c1270c84c218ccc9e67c1c318ef7eb6a4958f5e7
deepbiolab <lin_xiangtian@outlook.com>
2024-05-27 10:52:05 +0800

    update

---
+++ a.py
@@ -0,0 +1,39 @@
+def plot_pls_scores(pls_pipe, pc_x_axis=1, pc_y_axis=2):
+    X_columns = pls_pipe["features"].get_feature_names_out()
+    model = pls_pipe["model"]
+    fig = make_subplots(
+        rows=2,
+        cols=2,
+        specs=[[{"colspan": 2}, None], [{}, {}]],
+        subplot_titles=(
+            "Scores Plot ",
+            "Loadings of Principal Component - " + str(pc_x_axis),
+            "Loadings of Principal Component - " + str(pc_y_axis),
+        ),
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=model.x_scores_[:, pc_x_axis],
+            y=model.x_scores_[:, pc_y_axis],
+            mode="markers",
+            name="Scores",
+        ),
+        row=1,
+        col=1,
+    )
+    fig.add_bar(
+        x=X_columns,
+        y=model.x_loadings_[:, pc_x_axis - 1],
+        name="Loadings PC - " + str(pc_x_axis),
+        row=2,
+        col=[1, 2],
+    )
+    fig.add_bar(
+        x=X_columns,
+        y=model.x_loadings_[:, pc_y_axis - 1],
+        name="Loadings PC - " + str(pc_y_axis),
+        row=2,
+        col=2,
+    )
+    fig.update_layout(height=1000)
+    fig.show()
